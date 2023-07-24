c11c69538200defecd4cf8897d5a14eb66b06a72
Timothy Karani <kartimothy@gmail.com>
2023-07-24 10:31:47 +0300

    First of many

---
+++ elimisha_mwenzako/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for elimisha_mwenzako project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elimisha_mwenzako.settings')
+
+application = get_asgi_application()
---
+++ elimisha_mwenzako/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for elimisha_mwenzako project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elimisha_mwenzako.settings')
+
+application = get_wsgi_application()
